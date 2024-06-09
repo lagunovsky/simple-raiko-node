e9862672323153aa33381ccbaea8212a9dc8a68f
Ivan Lagunovsky <ivan.lagunovsky@gmail.com>
2024-06-09 17:20:43 +0200

    feat: log registration process output to file and modify parser to read from file

--- scripts/helpers/parse-forge-output.mjs
+++ scripts/helpers/parse-forge-output.mjs
@@ -1,4 +1,7 @@
-const [simulation, execution, conditions, error, success] = process.argv[2].split('==========================')
+import fs from 'fs'
+
+const output = fs.readFileSync(process.argv[2], 'utf-8')
+const [simulation, execution, conditions, error, success] = output.split('==========================')
 
 if (success) {
   const id = execution.split('\n').at(-4).slice(18).trim()
